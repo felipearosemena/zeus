80622fb9728cf9253f1a49a21fd5e6da5f82146a
Felipe Arosemena <felipe@briteweb.com>
2017-10-30 11:32:44 -0700

    - add https.Agent for local https cert validation - swap `http` module for `node-fetch` to perform requests (http requires host and path separated) - Remove conditional to check for post status. This filtering is done via php now

--- tests/e2e/healthcheck.spec.js
+++ tests/e2e/healthcheck.spec.js
@@ -1,28 +1,41 @@
-const http = require('http');
-const map = require('./config/map.json');
-const config = require('./config/site.json');
+const https = require('https')
+const fetch = require('node-fetch')
+const map = require('./config/map.json')
+const config = require('./config/site.json')
 
 let tests = {};
 
-map.forEach(function (post) {
+// Used for localhost https authentication
+const agent = new https.Agent({
+  rejectUnauthorized: false
+})
 
-  if (post.post_status === 'publish') {
+map
+  .forEach(function (post) {
 
     tests[`${post.post_title}`] = browser => {
+
       browser.url(post.guid).expect.element('body').to.be.present.before(500);
 
-      http.request({
-        host: post.guid,
-        // port: 80,
-        path: '',
-        method: "HEAD"
-      }, res => {
-        browser.assert.equal(res.statusCode, 200, 'Check status');
-      }).end()
+      browser.perform(done => {
+        fetch(post.guid, {
+          method: "HEAD",
+          // Need to pass the https agent for https requests on localhost
+          agent: post.guid.indexOf('https') > -1 ? agent : null
+        })
+        .then(res => {
+          browser.assert.equal(res.status, 200, 'Check status');
+          done()
+        })
+        // node-fetch requires handling of promise failure
+        .catch(err => {
+          browser.assert.fail(JSON.stringify(err), `Failed to perform fetch on ${post.guid}`)
+          done()
+        })
+      })
 
     }
-  }
 
-});
+  });
 
 module.exports = tests;
